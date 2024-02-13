8662cd6efe254cac8116670429cfa3b7f0062b4c
Jenna Marquardt <jsr0010@auburn.edu>
2024-02-13 14:13:01 -0600

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Jenna Marquardt on 2/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
