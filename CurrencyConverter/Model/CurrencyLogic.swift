c5772652ba7e105367f2e5a886fa25b5e5f87327
Jenna Marquardt <jsr0010@auburn.edu>
2024-02-16 17:55:50 -0600

    Added functionality.

---
+++ CurrencyConverter/Model/CurrencyLogic.swift
@@ -0,0 +1,119 @@
+//
+//  CurrencyLogic.swift
+//  CurrencyConverter
+//
+//  Created by Jenna Marquardt on 2/13/24.
+//
+
+import Foundation
+
+struct CurrencyLogic {
+    var euroSwitch = true
+    var poundSwitch = true
+    var ausDollarSwitch = true
+    var canDollarSwitch = true
+    var usd : Int = 0
+    var euro : Int = 0
+    var pound : Int = 0
+    var ausDollar : Int = 0
+    var canDollar : Int = 0
+    
+    enum TextError : Error {
+        case TextInvalid(reason: String)
+    }
+    
+    mutating func convert(_ value : Int) {
+        usd = value
+        //Euro 1 USD to .93
+        euro = Int(Double(value) * 0.93)
+        //Pound Sterling 1 USD to .79
+        pound = Int(Double(value) * 0.79)
+        //Australian Dollar 1 USD to 1.55
+        ausDollar = Int(Double(value) * 1.55)
+        // Canadian Dollar 1 USD to 1.36
+        canDollar = Int(Double(value) * 1.36)
+    }
+    
+    mutating func sendValue(_ text : String) -> (result: String, isValid: Bool) {
+        //Error checking for value typed in text field.
+        do {
+            try checkValue(text)
+            //If valid, update variables in currency logic
+            convert(Int(text)!)
+            return (String(usd), true)
+        } catch TextError.TextInvalid(let reason) {
+            //If invalid, return error string.
+            return (reason, false)
+        } catch {
+            return ("Error found", false)
+        }
+    }
+    
+    func checkValue(_ text : String) throws {
+        let integers: Set<Character> = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        for char in text {
+            if integers.contains(char) {
+            } else {
+                throw TextError.TextInvalid(reason: "The value must be an integer.")
+            }
+        }
+    }
+    
+    func getAmount() -> Int {
+        return usd
+    }
+    
+    func getSwitchArray() -> Array<Bool> {
+        return [euroSwitch, poundSwitch, ausDollarSwitch, canDollarSwitch]
+    }
+    
+    func getConversions() -> Array<Int> {
+        return [euro, pound, ausDollar, canDollar]
+    }
+    
+    //    mutating func setSwitch(_ switchValue : Bool, _ switchType : String) {
+    //        switch switchType {
+    //        case "euro":
+    //            euroSwitch = switchValue
+    //        case "pound":
+    //            poundSwitch = switchValue
+    //        case "aus":
+    //            ausDollarSwitch = switchValue
+    //        default:
+    //            canDollarSwitch = switchValue
+    //        }
+    //    }
+    
+    mutating func setEuroSwitch(_ switchValue : Bool) {
+        if switchValue {
+            euroSwitch = true
+        } else {
+            euroSwitch = false
+        }
+    }
+    
+    mutating func setPoundSwitch(_ switchValue : Bool) {
+        if switchValue {
+            poundSwitch = true
+        } else {
+            poundSwitch = false
+        }
+    }
+    
+    mutating func setAusDollarSwitch(_ switchValue : Bool) {
+        if switchValue {
+            ausDollarSwitch = true
+        } else {
+            ausDollarSwitch = false
+        }
+    }
+    
+    mutating func setCanDollarSwitch(_ switchValue : Bool) {
+        if switchValue {
+            canDollarSwitch = true
+        } else {
+            canDollarSwitch = false
+        }
+    }
+    
+}
---
+++ CurrencyConverter/View/CurrencyAmountView.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyAmountView.swift
+//  CurrencyConverter
+//
+//  Created by Jenna Marquardt on 2/13/24.
+//
+
+import UIKit
+
+class CurrencyAmountView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
